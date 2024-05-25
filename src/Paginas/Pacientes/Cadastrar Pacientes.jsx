1fee015cf8eae270f3beb7b3ab5ef52f71f84893
Caio Basso <caiobasso95@gmail.com>
2024-05-25 18:20:48 -0300

    Cadastro

---
+++ src/Paginas/Pacientes/Cadastrar Pacientes.jsx
@@ -0,0 +1,14 @@
+import { Card, Col, Container, Form, Row } from "react-bootstrap";
+import { useOutletContext } from "react-router-dom";
+import "./CadastrarPacientes.css";
+
+function Pacientes() {
+    const { show } = useOutletContext();
+    return (
+      <div>
+        <Container className={`container-pacientes ${show ? "container-pacientes-active" : ""}`}>
+
+        </Container>
+      </div>
+  );
+}
