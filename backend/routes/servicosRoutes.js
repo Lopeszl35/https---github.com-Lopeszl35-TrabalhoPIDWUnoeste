0b14ef9f0366ff148c266c0a06c4e365589898d8
rafael lopes <rafalopes900@hotmail.com>
2024-06-02 20:54:30 -0300

    delete

--- backend/routes/servicosRoutes.js
+++
@@ -1,8 +0,0 @@
-const ServicoController = require('../controller/servicoController');
-const servicoController = new ServicoController;
-const express = require('express');
-const router = express.Router();
-
-router.get('/servicos', servicoController.obterTodos);
-
-module.exports = router;
